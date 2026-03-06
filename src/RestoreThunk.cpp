e945860e37d213c95f5b139aa470b60ccd6172b4
wildcherry2 <tylercwray@gmail.com>
2026-03-06 13:45:23 -0500

    use dynamic structure in restore thunk

--- src/RestoreThunk.cpp
+++ src/RestoreThunk.cpp
@@ -1,13 +1,6 @@
 #include "RestoreThunk.hpp"
 #include "Context.hpp"
 
-struct RestoreThunkLocals {
-    uint64_t IncomingIntArgs[4];
-    uint64_t NonVolatileIntRegs[4];
-    Xmm IncomingFloatArgs[10];
-    Xmm NonVolatileFloatRegs[7];
-};
-
 FThunkPtr GenerateRestoreThunk(void* CallTo, FuncSignature Signature) {
     using namespace asmjit;
     using namespace asmjit::x86;
@@ -20,19 +13,40 @@ FThunkPtr GenerateRestoreThunk(void* CallTo, FuncSignature Signature) {
     FuncArgInfo ArgInfo { Signature };
 
     // allocate locals with shadow space for calls (call to 'CallTo' will always take the most space so we use ArgInfo.Detail().arg_stack_size())
-    // we'll need space for all nonvolatile registers and argument registers in addition to the shadow/arg space.
-    const auto LocalsOffset = ArgInfo.Detail().arg_stack_size() + (sizeof(RestoreThunkLocals) + ArgInfo.Detail().arg_stack_size() % 16 == 8 ? 0 : 8);
-    TheAssembler.sub(rsp, sizeof(RestoreThunkLocals) + LocalsOffset);
-    const auto InitialRspOffset = sizeof(RestoreThunkLocals) + LocalsOffset;
+    // we'll need space for all nonvolatile registers and argument registers in addition to the shadow/arg space + alignment.
+    const auto ShadowArgSpace = ArgInfo.Detail().arg_stack_size();
+    const auto NVIntRegSpace = GetNonVolatileGpRegs().size() * 8;
+    const auto NVFltRegSpace = GetNonVolatileVecRegs().size() * 16;
+    const auto IntArgSpace = ArgInfo.GetArgumentIntegralRegisters().size() * 8;
+    const auto FltArgSpace = ArgInfo.GetArgumentFloatingRegisters().size() * 16;
+    auto SumSpace = ShadowArgSpace + NVIntRegSpace + NVFltRegSpace + IntArgSpace + FltArgSpace;
+    SumSpace += SumSpace % 16 == 8 ? 0 : 8;
+    TheAssembler.sub(rsp, SumSpace);
+    /*
+     * Roughly:
+     * struct Locals {
+     *      ShadowArgSpace  // offset 0
+     *      IntArgSpace     // offset sizeof ShadowArgSpace
+     *      FltArgSpace     // offset sizeof ShadowArgSpace + sizeof IntArgSpace
+     *      NVIntRegSpace   // offset sizeof ShadowArgSpace + sizeof IntArgSpace + sizeof FltArgSpace
+     *      NVFltRegSpace   // offset sizeof ShadowArgSpace + sizeof IntArgSpace + sizeof FltArgSpace + NVIntRegSpace
+     * }
+     */
+    const auto IntArgPtr = ptr(rsp, ShadowArgSpace);                  // treat as an array of uint64_t
+    const auto FltArgPtr = IntArgPtr.clone_adjusted(IntArgSpace);          // treat as an array of Xmm
+    const auto NVIntRegPtr = FltArgPtr.clone_adjusted(FltArgSpace);        // treat as an array of uint64_t
+    const auto NVFltRegPtr = NVIntRegPtr.clone_adjusted(NVIntRegSpace);    // treat as an array of Xmm
+    const auto RspInitial = NVFltRegPtr.clone_adjusted(NVFltRegSpace);     // pointer to what rsp was at the beginning of the function
+
 
     // we need to call RegisterContextStack::Top, so we need to save the registers with arguments first
     {
         int IntIndex = 0, FloatIndex = 0;
         for (auto& Arg: ArgInfo.GetArgumentIntegralRegisters()) {
-            TheAssembler.mov(ptr(rsp, offsetof(RestoreThunkLocals, IncomingIntArgs) + (8 * IntIndex++) + LocalsOffset), Arg);
+            TheAssembler.mov(IntArgPtr.clone_adjusted(8 * IntIndex++), Arg);
         }
         for (auto& Arg: ArgInfo.GetArgumentFloatingRegisters()) {
-            TheAssembler.movdqu(ptr(rsp, offsetof(RestoreThunkLocals, IncomingIntArgs) + (16 * FloatIndex++) + LocalsOffset), Arg);
+            TheAssembler.movdqu(FltArgPtr.clone_adjusted(16 * FloatIndex++), Arg);
         }
     }
 
@@ -43,21 +57,26 @@ FThunkPtr GenerateRestoreThunk(void* CallTo, FuncSignature Signature) {
     {
         int Index = 0;
         for (auto& Register : GetNonVolatileGpRegs()) {
-            TheAssembler.mov(ptr(rsp, offsetof(RestoreThunkLocals, NonVolatileIntRegs) + (8 * Index++) + LocalsOffset), Register);
+            TheAssembler.mov(NVIntRegPtr.clone_adjusted(8 * Index++), Register);
         }
         Index = 0;
         for (auto& Register : GetNonVolatileVecRegs()) {
-            TheAssembler.movdqu(ptr(rsp, offsetof(RestoreThunkLocals, NonVolatileFloatRegs) + (16 * Index++) + LocalsOffset), Register);
+            TheAssembler.movdqu(NVFltRegPtr.clone_adjusted(16 * Index++), Register);
         }
     }
 
     // move arguments on the stack to new stack slot for the call (already allocated)
     for (auto& Arg: ArgInfo.GetArguments()) {
         if (Arg.is_stack()) {
-            TheAssembler.mov(r11, ptr(rsp, InitialRspOffset + Arg.stack_offset()));
+            TheAssembler.mov(r11, RspInitial.clone_adjusted(Arg.stack_offset()));
             TheAssembler.mov(ptr(rsp, Arg.stack_offset()), r11);
         }
     }
+
+    // restore rflags
+    TheAssembler.mov(r11, ptr(rax, offsetof(RegisterContext, rflags)));
+    TheAssembler.push(r11);
+    TheAssembler.popfq();
 
     // restore all saved registers that aren't args
     const auto GpMask = ArgInfo.GpRegMask();
@@ -80,10 +99,10 @@ FThunkPtr GenerateRestoreThunk(void* CallTo, FuncSignature Signature) {
     {
         int IntIndex = 0, FloatIndex = 0;
         for (auto& Arg: ArgInfo.GetArgumentIntegralRegisters()) {
-            TheAssembler.mov(Arg, ptr(rsp, offsetof(RestoreThunkLocals, IncomingIntArgs) + (8 * IntIndex++) + LocalsOffset));
+            TheAssembler.mov(Arg, IntArgPtr.clone_adjusted(8 * IntIndex++));
         }
         for (auto& Arg: ArgInfo.GetArgumentFloatingRegisters()) {
-            TheAssembler.movdqu(Arg, ptr(rsp, offsetof(RestoreThunkLocals, IncomingIntArgs) + (16 * FloatIndex++) + LocalsOffset));
+            TheAssembler.movdqu(Arg, FltArgPtr.clone_adjusted(16 * FloatIndex++));
         }
     }
 
@@ -94,16 +113,16 @@ FThunkPtr GenerateRestoreThunk(void* CallTo, FuncSignature Signature) {
     {
         int Index = 0;
         for (auto& Register : GetNonVolatileGpRegs()) {
-            TheAssembler.mov(Register, ptr(rsp, offsetof(RestoreThunkLocals, NonVolatileIntRegs) + (8 * Index++) + LocalsOffset));
+            TheAssembler.mov(Register, NVIntRegPtr.clone_adjusted(8 * Index++));
         }
         Index = 0;
         for (auto& Register : GetNonVolatileVecRegs()) {
-            TheAssembler.movdqu(Register, ptr(rsp, offsetof(RestoreThunkLocals, NonVolatileFloatRegs) + (16 * Index++) + LocalsOffset));
+            TheAssembler.movdqu(Register, NVFltRegPtr.clone_adjusted(16 * Index++));
         }
     }
 
     // now we're done, deallocate and return
-    TheAssembler.add(rsp, InitialRspOffset);
+    TheAssembler.add(rsp, SumSpace);
     TheAssembler.ret();
 
     if (TheAssembler.finalize() != Error::kOk) return nullptr;
